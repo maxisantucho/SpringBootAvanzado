6efe0df736d2cdd0bf9e2318ecdeb2f05266d5eb
Maxi Santucho <maximilianosantucho@outlook.es>
2025-06-15 23:21:34 -0300

    link Swagger

--- mi-primer-api-rest/src/main/java/com/stc/ec/MiPrimerApiRestApplication.java
+++ mi-primer-api-rest/src/main/java/com/stc/ec/MiPrimerApiRestApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MiPrimerApiRestApplication {
 
+	//http://localhost:8080/swagger-ui/index.html
+
 	public static void main(String[] args) {
 		SpringApplication.run(MiPrimerApiRestApplication.class, args);
 	}
